962c9d13e5e2fac715d9d0a2f00345f15e10e5a2
System Malt <javierorfo@protonmail.com>
2023-03-26 12:31:57 -0300

    Update fuel.lua

--- plugin/fuel.lua
+++ plugin/fuel.lua
@@ -1,7 +1,7 @@
--- #####################################################
--- # Maintainer: Javier Orfo                           #
--- # URL:        https://github.com/javiorfo/nvim-fuel #
--- #####################################################
+-- #######################################################
+-- # Maintainer: System Malt                             #
+-- # URL:        https://github.com/systemmalt/nvim-fuel #
+-- #######################################################
 
 if vim.g.fuel then
   return
