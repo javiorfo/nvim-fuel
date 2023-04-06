19b82c1857ba941980fc06df7786a6d5ea23e437
Whisky & Code <javierorfo@protonmail.com>
2023-04-06 19:24:19 -0300

    Update scala.lua

--- ftplugin/scala.lua
+++ ftplugin/scala.lua
@@ -1,6 +1,6 @@
--- #####################################################
--- # Maintainer: Javier Orfo                           #
--- # URL:        https://github.com/javiorfo/nvim-fuel #
--- #####################################################
+-- ######################################################
+-- # Maintainer: Javier Orfo                            #
+-- # URL:        https://github.com/whiskoder/nvim-fuel #
+-- ######################################################
 
 vim.cmd[[autocmd BufEnter,BufNewFile,BufRead *.scala,*.sc lua vim.b.fuel_language = "scala"]]
