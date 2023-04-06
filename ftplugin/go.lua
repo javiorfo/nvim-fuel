142f983a59baecf2bfa12fe2924113d96dd0485d
Whisky & Code <javierorfo@protonmail.com>
2023-04-06 19:23:35 -0300

    Update go.lua

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -1,6 +1,6 @@
--- #####################################################
--- # Maintainer: Javier Orfo                           #
--- # URL:        https://github.com/javiorfo/nvim-fuel #
--- #####################################################
+-- ######################################################
+-- # Maintainer: Javier Orfo                            #
+-- # URL:        https://github.com/whiskoder/nvim-fuel #
+-- ######################################################
 
 vim.cmd[[autocmd BufEnter,BufNewFile,BufRead *.go lua vim.b.fuel_language = "go"]]
