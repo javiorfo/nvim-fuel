d6a60c8216dad65d1b697196487c2869ec229451
Mr. Charkuils <javierorfo@protonmail.com>
2023-04-10 12:13:15 -0300

    Update lua.lua

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,6 +1,6 @@
 -- ######################################################
--- # Maintainer: Javier Orfo                            #
--- # URL:        https://github.com/whiskoder/nvim-fuel #
+-- # Maintainer: Mr. Charkuils                          #
+-- # URL:        https://github.com/charkuils/nvim-fuel #
 -- ######################################################
 
 vim.cmd[[autocmd BufEnter,BufNewFile,BufRead *.lua lua vim.b.fuel_language = "lua"]]
