3a8d56d6eb32eba34df9e3e6156cfcfb493a5bb5
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:32:46 -0300

    Update rust.lua

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,6 +1,1 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-fuel #
--- ######################################################
-
 vim.cmd[[autocmd BufEnter,BufNewFile,BufRead *.rs lua vim.b.fuel_language = "rust"]]
