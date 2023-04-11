33bfbbe6e5346b51396f6b4f5a55d7e26fee49d9
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:32:16 -0300

    Update kotlin.lua

--- ftplugin/kotlin.lua
+++ ftplugin/kotlin.lua
@@ -1,6 +1,1 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-fuel #
--- ######################################################
-
 vim.cmd[[autocmd BufEnter,BufNewFile,BufRead *.kt,*.kts lua vim.b.fuel_language = "kotlin"]]
