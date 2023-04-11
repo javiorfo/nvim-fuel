e1e12e658cfca6e7726f128e266fa262d0f322ff
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:32:37 -0300

    Update python.lua

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,6 +1,1 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-fuel #
--- ######################################################
-
 vim.cmd[[autocmd BufEnter,BufNewFile,BufRead *.py lua vim.b.fuel_language = "python"]]
