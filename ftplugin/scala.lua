3fa57bb8b4c3d6d882ce3c1f8ea08c383fa051cd
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:32:55 -0300

    Update scala.lua

--- ftplugin/scala.lua
+++ ftplugin/scala.lua
@@ -1,6 +1,1 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-fuel #
--- ######################################################
-
 vim.cmd[[autocmd BufEnter,BufNewFile,BufRead *.scala,*.sc lua vim.b.fuel_language = "scala"]]
