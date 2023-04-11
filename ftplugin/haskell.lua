06c868c6482ca69ae252bcde8295ee134531cd48
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:31:52 -0300

    Update haskell.lua

--- ftplugin/haskell.lua
+++ ftplugin/haskell.lua
@@ -1,6 +1,1 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-fuel #
--- ######################################################
-
 vim.cmd[[autocmd BufEnter,BufNewFile,BufRead *.hs,*.lhs lua vim.b.fuel_language = "haskell"]]
