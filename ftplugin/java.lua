a99ccee1e6d679ccd8d2ad39e1706f6f9805c82e
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:32:06 -0300

    Update java.lua

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,6 +1,1 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-fuel #
--- ######################################################
-
 vim.cmd[[autocmd BufEnter,BufNewFile,BufRead *.java lua vim.b.fuel_language = "java"]]
