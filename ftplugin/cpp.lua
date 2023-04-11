d799a68d3692626dcd49842c48ad9897e2500121
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:31:23 -0300

    Update cpp.lua

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,6 +1,1 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-fuel #
--- ######################################################
-
 vim.cmd[[autocmd BufEnter,BufNewFile,BufRead *.cpp lua vim.b.fuel_language = "cpp"]]
