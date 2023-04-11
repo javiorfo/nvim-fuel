81e836343171b2b86f2b7f50d83228c39f910b1f
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:34:16 -0300

    Update haskell.lua

--- lua/fuel/lang/haskell.lua
+++ lua/fuel/lang/haskell.lua
@@ -1,8 +1,3 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-fuel #
--- ######################################################
-
 local util = require'fuel.util'
 
 return {
