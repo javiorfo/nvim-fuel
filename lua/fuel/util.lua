69a9b99dc16db70732b6fa133de39f286e618b88
Javier Orfo <javierorfo@protonmail.com>
2023-05-01 18:20:54 -0300

    fix icon

--- lua/fuel/util.lua
+++ lua/fuel/util.lua
@@ -25,11 +25,11 @@ function M.find_language_by_compiler(compiler)
 end
 
 function M.statusline_style(lang, file)
-    return string.format("%%#Normal# %s Console | Running   %s", lang, file)
+    return string.format("%%#Normal# %s Console | Running 󱦰 %s", lang, file)
 end
 
 function M.footer(lang, file)
-    return string.format("%s Console   %s", lang, file)
+    return string.format("%s Console 󱦰 %s", lang, file)
 end
 
 return M
