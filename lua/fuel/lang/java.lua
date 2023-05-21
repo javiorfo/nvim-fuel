be60edb212b8440e6329a3ec27a37e4f0a960188
Javier Orfo <javierorfo@protonmail.com>
2023-05-21 19:54:54 -0300

    java -Xdiags:verbose

--- lua/fuel/lang/java.lua
+++ lua/fuel/lang/java.lua
@@ -6,7 +6,7 @@ return {
         local dot_classes = classpath .. "/*.class"
 
         vim.cmd("autocmd BufDelete java_fuel_main_console silent !rm -f " .. dot_classes)
-        return string.format(" javac %s -Xlint:all && java -cp %s %s && rm -f %s 2> /dev/null", file_with_extension, classpath, file, dot_classes)
+        return string.format(" javac %s -Xlint:all -Xdiags:verbose && java -cp %s %s && rm -f %s 2> /dev/null", file_with_extension, classpath, file, dot_classes)
     end,
     get_statusline = function(file)
         return util.statusline_style(" Java", file)
