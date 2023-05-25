89915c805a39529d7678ca8b7c5da9fea1ee170b
Javier Orfo <javierorfo@protonmail.com>
2023-05-25 12:24:52 -0300

    gcc -Wall flag

--- lua/fuel/lang/haskell.lua
+++ lua/fuel/lang/haskell.lua
@@ -7,10 +7,10 @@ return {
         return string.format(" ghc -dynamic -v0 %s && %s && rm -f %s.o %s.hi %s 2> /dev/null", file_with_extension, file, file, file, file)
     end,
     get_statusline = function(file)
-        return util.statusline_style(" Haskell", file)
+        return util.statusline_style("  Haskell", file)
     end,
     get_footer = function(file)
-        return util.footer(" Haskell", file)
+        return util.footer("  Haskell", file)
     end
 
 }
