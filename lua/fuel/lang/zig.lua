8d2a3ed168fb15cd0cadc7ccc01a2257c0c33913
Javier Orfo <javierorfo@protonmail.com>
2023-07-07 17:20:25 -0300

    zig errors

--- lua/fuel/lang/zig.lua
+++ lua/fuel/lang/zig.lua
@@ -2,7 +2,7 @@ local util = require'fuel.util'
 
 return {
     build = function (file_with_extension)
-        return string.format(" zig run %s 2> /dev/null", file_with_extension)
+        return string.format(" zig run %s", file_with_extension)
     end,
     get_statusline = function(file)
         return util.statusline_style(" Zig", file)
