6a77e744136fb21fb18b7ef0983942338cf358f0
Javier Orfo <javierorfo@protonmail.com>
2023-07-10 17:41:56 -0300

    go change

--- lua/fuel/lang/go.lua
+++ lua/fuel/lang/go.lua
@@ -2,7 +2,7 @@ local util = require'fuel.util'
 
 return {
     build = function (file_with_extension)
-        return string.format(" go run %s 2> /dev/null", file_with_extension)
+        return string.format(" go run %s", file_with_extension)
     end,
     get_statusline = function(file)
         return util.statusline_style(" Go", file)
