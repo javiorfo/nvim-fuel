9a8bf2a2723c1e4121b3fcb31172ce6c5bd6d662
Javier Orfo <javierorfo@prontonmail.com>
2024-01-26 16:54:53 -0300

    -Werror added on C compilation

--- lua/fuel/lang/c.lua
+++ lua/fuel/lang/c.lua
@@ -4,7 +4,7 @@ return {
     build = function (file_with_extension, file)
         file = vim.fn.expand("%:h") .. "/" .. file
         vim.cmd("autocmd BufDelete c_fuel_main_console silent !rm -f " .. file)
-        return string.format(" gcc -Wall %s -o %s && %s && rm -f %s 2> /dev/null", file_with_extension, file, file, file)
+        return string.format(" gcc -Wall -Werror %s -o %s && %s && rm -f %s 2> /dev/null", file_with_extension, file, file, file)
     end,
     get_statusline = function(file)
         return util.statusline_style(" C", file)
