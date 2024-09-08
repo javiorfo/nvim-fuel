bc70f628f3e3ae59c51e9e600b45bca15c2fc3eb
Javier Orfo <javierorfo@prontonmail.com>
2024-09-08 10:05:25 -0300

    haskell bug

--- lua/fuel/lang/haskell.lua
+++ lua/fuel/lang/haskell.lua
@@ -3,7 +3,7 @@ local util = require'fuel.util'
 return {
     build = function (file_with_extension, file)
         vim.cmd(string.format("autocmd BufDelete haskell_fuel_main_console silent !rm -f /tmp/%s.o /tmp/%s.hi /tmp/%s", file, file, file))
-        return string.format("ghc -dynamic -v0 %s && /tmp/%s && rm -f /tmp/%s.o /tmp/%s.hi /tmp/%s 2> /dev/null", file_with_extension, file, file, file, file)
+        return string.format("ghc -dynamic -v0 -o /tmp/%s %s && /tmp/%s && rm -f /tmp/%s.o /tmp/%s.hi /tmp/%s 2> /dev/null", file, file_with_extension, file, file, file, file)
     end,
     get_statusline = function(file)
         return util.statusline_style("  Haskell", file)
