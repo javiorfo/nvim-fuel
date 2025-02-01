e36d438a402d185aee4884a84875ec2195f87aab
Javier Orfo <javierorfo@prontonmail.com>
2025-02-01 13:45:46 -0300

    zig icon

--- lua/fuel/lang/zig.lua
+++ lua/fuel/lang/zig.lua
@@ -5,9 +5,9 @@ return {
         return string.format("zig run %s", file_with_extension)
     end,
     get_statusline = function(file)
-        return util.statusline_style(" Zig", file)
+        return util.statusline_style(" Zig", file)
     end,
     get_footer = function(file)
-        return util.footer(" Zig", file)
+        return util.footer(" Zig", file)
     end
 }
