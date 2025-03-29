9c0789bc8f463cf3a5b702a376d215b3af330126
javiorfo <javierorfo@prontonmail.com>
2025-03-29 10:43:28 -0300

    rustc to cargo

--- lua/fuel/lang/rust.lua
+++ lua/fuel/lang/rust.lua
@@ -1,9 +1,8 @@
 local util = require'fuel.util'
 
 return {
-    build = function (file_with_extension, file)
-        vim.cmd("autocmd BufDelete rust_fuel_main_console silent !rm -f /tmp/" .. file)
-        return string.format("rustc --edition 2021 --out-dir /tmp %s && /tmp/%s && rm -f /tmp/%s 2> /dev/null", file_with_extension, file, file)
+    build = function (file_with_extension)
+        return string.format("cargo run %s", file_with_extension)
     end,
     get_statusline = function(file)
         return util.statusline_style(" Rust", file)
